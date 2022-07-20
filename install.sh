74a83e3558a283d3e8629b65ddd755cfe03e6bb5
Lorenzo Antonelli <lorenzoantonelli@outlook.com>
2022-07-20 15:06:58 +0200

    Renamed temp folder from ~/tmp to ~/tmp_modelica

--- install.sh
+++ install.sh
@@ -1,14 +1,15 @@
+#! /usr/bin/env bash
 echo "deb https://build.openmodelica.org/apt `lsb_release -cs` nightly" | sudo tee /etc/apt/sources.list.d/openmodelica.list
 echo "deb-src https://build.openmodelica.org/apt nightly contrib" | sudo tee -a /etc/apt/sources.list.d/openmodelica.list
 wget -q http://build.openmodelica.org/apt/openmodelica.asc -O- | sudo apt-key add - 
 sudo apt update
 sudo apt install devscripts debhelper
-mkdir ~/tmp
-cd ~/tmp
+mkdir ~/tmp_modelica
+cd ~/tmp_modelica
 sudo apt build-dep openmodelica
 apt -b source openmodelica
 sudo dpkg -i *.deb
 echo "OpenModelica installed, cleaning up..."
 cd ..
-rm -rf ~/tmp
+rm -rf ~/tmp_modelica
 exit 0
